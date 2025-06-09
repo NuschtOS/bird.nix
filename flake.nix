a273a92c21aa42b08d361885f19f860152b70d2f
Sandro <sandro.jaeckel@gmail.com>
2025-06-09 22:46:58 +0200

    Check unstable and stable (#10)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
